87bdf775d53479d134e42b96ec07236621cf7562
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-17 20:05:30 +0300

    *Added blog constatants

---
+++ src/store/constants/blogsConstants.js
@@ -0,0 +1,12 @@
+// export blog constants
+// fetch blog constants
+export const BLOGS_REQUEST = "BLOGS_REQUEST";
+export const BLOGS_SUCCESS = "BLOGS_SUCCESS";
+export const BLOGS_FAIL = "BLOGS_FAIL";
+
+// create blog constants
+
+export const BLOG_CREATE_REQUEST = "BLOG_CREATE_REQUEST";
+export const BLOG_CREATE_SUCCESS = "BLOG_CREATE_SUCCESS";
+export const BLOG_CREATE_FAIL = "BLOG_CREATE_FAIL";
+export const BLOG_CREATE_RESET = "BLOG_CREATE_RESET";
