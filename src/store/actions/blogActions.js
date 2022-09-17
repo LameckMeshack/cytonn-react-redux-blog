7e7785d1b62e7954964fba7fb4b1d6727e7b91ff
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-18 00:53:09 +0300

    *Added single blog actions

--- src/store/actions/blogActions.js
+++ src/store/actions/blogActions.js
@@ -4,8 +4,12 @@ import {
   BLOGS_FAIL,
   BLOGS_REQUEST,
   BLOGS_SUCCESS,
+  BLOG_FAIL,
+  BLOG_SUCCESS,
+  BLOG_REQUEST,
 } from "../constants/blogsConstants";
 
+// fetching all blogs
 export const fetchBlogs = () => async (dispatch) => {
   try {
     dispatch({ type: BLOGS_REQUEST });
@@ -20,6 +24,29 @@ export const fetchBlogs = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: BLOGS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// fetching single blog
+export const fetchBlog = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: BLOG_REQUEST });
+
+    const { data } = await axios.get(`http://localhost:3000/blogs/${id}`);
+    // console.log(data);
+
+    dispatch({
+      type: BLOG_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: BLOG_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
