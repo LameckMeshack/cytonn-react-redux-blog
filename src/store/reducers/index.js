e1fa708704ba7e76e300ed7bd060033c5e88b087
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-18 19:43:30 +0300

    *Impported auth reducer to combined reducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import authReducer from "./authReducers";
 import { fetchBlogReducer, fetchBlogsReducer } from "./blogReducers";
 
 //combining reducers
 export const rootReducer = combineReducers({
   blogList: fetchBlogsReducer,
   blog: fetchBlogReducer,
+  userInfo: authReducer,
 });
