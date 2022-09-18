1f36e214fc5ddda70b1a8cdb08c22e117eb9531f
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-18 12:12:16 +0300

    *Added create form

---
+++ src/components/CreateForm.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+function CreateForm() {
+  return (
+    <div className="block p-6 rounded-lg shadow-lg mt-6 mx-auto bg-white max-w-md">
+      <form>
+        <div className="form-group mb-6">
+          <input
+            type="text"
+            className="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            placeholder="Title"
+          />
+        </div>
+        <div className="form-group mb-6">
+          <input
+            type="link"
+            className="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            placeholder="Image Link"
+          />
+        </div>
+        <div className="form-group mb-6">
+          <textarea
+            className="
+        form-control
+        block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+      "
+            rows="8"
+            placeholder="blog body"
+          ></textarea>
+        </div>
+
+        <button
+          type="submit"
+          className="
+      w-full
+      px-6
+      py-2.5
+      bg-blue-600
+      text-white
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      rounded
+      shadow-md
+      hover:bg-blue-700 hover:shadow-lg
+      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+      active:bg-blue-800 active:shadow-lg
+      transition
+      duration-150
+      ease-in-out"
+        >
+          create
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateForm;
